7f019f3dc659dd481ec325d4dafe9fc8dba6b477
magisterblaman <hannes.blaman@lbs.se>
2024-09-02 14:00:58 +0200

    While loop

--- AW23_PRRPRR01_04_Iterationer/Program.cs
+++ AW23_PRRPRR01_04_Iterationer/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_04_Iterationer {
 	internal class Program {
 		static void Main(string[] args) {
+
+			int myInt = 43;
+
+			bool run = true;
+
+			while (run) {
+				Console.WriteLine("Vill du köra en gång till?");
+				string answer = Console.ReadLine().ToLower();
+
+				if (answer == "nej") {
+					run = false;
+				}
+			}
+
 		}
 	}
 }
